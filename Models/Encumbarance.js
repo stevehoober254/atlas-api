e1c5030995e21c070626c28fa0feabcc5ef3c6d5
RonexLemon <ronexondimu@gmail.com>
2024-03-13 15:59:07 +0300

    encumbrance schema

--- Models/Encumbarance.js
+++ Models/Encumbarance.js
@@ -1,10 +1,3 @@
-/** linked to user,property
- * Models *****
- * fields ****
- * Dispute
- * 
- */
-
 
 
 
@@ -13,52 +6,40 @@
 const mongoose = require("mongoose")
 const {Schema} = mongoose
 
-const PropertyTransferSchema = new Schema(
+const EncumbranceSchema = new Schema(
     {
       user: {
         type: Schema.Types.ObjectId,  //ref owner of the property
         ref: "User",
         required: true,
       },
-      property: {
-        type: Schema.Types.ObjectId,   //ref to the property that is being transferd
-        ref: "EnlistProperty",
-        required: true,
-      },
+      
       landReferenceNumber: {
         type: String,
         unique:true,
         required: true,
       },
-      newOwnerEthereumAddress: {
-        type: String,
-        required: true,
-      },
-      requestorAddress: {
+      date: {
+         type: Date,
+          default: Date.now },
+      encumbrances: {
         type: String,
         required: false,
       },
-      approvalDate: {
-        type: Date,
-        required: true,
-      },
-      
-      requestDate: {
-        type: Date, 
-        required: false,
-      },
-     
+
       status: {
           type: String,
-          enum: ['Requested', 'Approved', 'Encumbered', 'Error'],
-          default: "Requested",
+          enum: ['Added', 'Removed'],
+          default: "Added",
         },
     },
+
+
    
     { timestamps: true }
   );
   
-  module.exports = mongoose.models.PropertyTranfer|| mongoose.model("PropertyTranfer", PropertyTransferSchema);
+  module.exports = mongoose.models.Encumbrance|| mongoose.model("Encumbrance", EncumbranceSchema);
 
 
 
