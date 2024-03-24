0f918e1f9c6a336787440eeacb7c46e01bfa0c8e
RonexLemon <ronexondimu@gmail.com>
2024-03-24 22:48:55 +0300

    remove: debug messages

--- upload/uploadDocuments.js
+++ upload/uploadDocuments.js
@@ -10,10 +10,10 @@ const uploadImage = (image) => {
   return new Promise((resolve, reject) => {
     cloudinary.uploader.upload(image, opts, (error, result) => {
       if (result && result.secure_url) {
-        console.log(result.secure_url);
+        //console.log(result.secure_url);
         return resolve(result.secure_url);
       }
-      console.log(error.message);
+      //console.log(error.message);
       return reject({ message: error.message });
     });
   });
