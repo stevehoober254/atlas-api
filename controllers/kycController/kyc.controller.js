13aba5c4a1a340da55ba2d60a3f9e24cc73a2068
Stephen Gachoka <7832567-stevehoober254@users.noreply.gitlab.com>
2024-06-06 18:58:45 +0300

    kra pin verification

--- controllers/kycController/kyc.controller.js
+++ controllers/kycController/kyc.controller.js
@@ -7,35 +7,21 @@ const PARTNER_ID = process.env.SMILE_ID_PARTNER_ID;
 const DEFAULT_CALLBACK = process.env.SMILE_ID_CALLBACK_URL;
 const API_KEY = process.env.SMILE_ID_API_KEY;
 const SID_SERVER = 1; // 0 for sandbox
-
+const partner_params = {
+    job_id: "04984209-fea7-4c1a-a5fd-976ad57cbdb6",
+    user_id: "10e3b22c-905c-4f37-933a-dbca617151e7",
+    job_type: 5,
+};
 
 const verifyIDNumber = asyncHandler(async (req, res) => {
     try {
         const id_number = req.body.id_number
-        const partner_params = {
-            job_id: "04984209-fea7-4c1a-a5fd-976ad57cbdb6",
-            user_id: "10e3b22c-905c-4f37-933a-dbca617151e7",
-            job_type: 5,
-            id_number: id_number
-        };
+
         const id_info = {
             country: 'KE', // The country where ID document was issued
             id_type: 'NATIONAL_ID', // The ID document type
             id_number: id_number
         };
-        const options = {
-            // Set to true if you want to get the job result in sync (in addition to the result been sent to
-            // your callback). If set to false, result is sent to callback url only.
-            return_job_status: true,
-            // Set to true to return results of all jobs you have ran for the user in addition to current job
-            // result. You must set return_job_status to true to use this flag.
-            return_history: false,
-            // Set to true to receive selfie and liveness images you uploaded. You must set return_job_status
-            // to true to use this flag.
-            signature: true,
-        };
-
-
         const connection = new SmileID.IDApi(
             PARTNER_ID,
             API_KEY,
@@ -54,6 +40,37 @@ const verifyIDNumber = asyncHandler(async (req, res) => {
 
 })
 
+const verifyKRANumber = asyncHandler(async (req, res) => {
+    try {
+        const kra_pin = req.body.kra_pin;
+
+        const id_info = {
+            country: 'KE', // The country where ID document was issued
+            id_type: 'KRA_PIN', // The ID document type
+            id_number: kra_pin,
+            entered: true,
+            postal_address: '',
+            postal_code: '',
+            citizenship: 'Kenyan'
+        };
+        const connection = new SmileID.IDApi(
+            PARTNER_ID,
+            API_KEY,
+            SID_SERVER,
+        )
+        const result = await connection.submit_job(
+            partner_params,
+            id_info
+        );
+        console.info(result);
+        res.status(200).send(result)
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error)
+    }
+
+})
+
 // Callback Endpoint
 const smileIDCallback = asyncHandler(async (req, res) => {
     const smileid_response = req.body;
@@ -63,4 +80,5 @@ const smileIDCallback = asyncHandler(async (req, res) => {
 
 })
 
-module.exports = { smileIDCallback, verifyIDNumber }
+
+module.exports = { smileIDCallback, verifyIDNumber, verifyKRANumber }
--- routes/kyc/route.js
+++ routes/kyc/route.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { smileIDCallback, verifyIDNumber } = require('../../controllers/kycController/kyc.controller');
+const { smileIDCallback, verifyIDNumber, verifyKRANumber } = require('../../controllers/kycController/kyc.controller');
 const router = express.Router();
 
 // Route to verify ID
 router.post('/verify/id', verifyIDNumber)
+
+// Route to verify KRA Pin
+router.post('/verify/kra', verifyKRANumber)
 
 // Route to process SmileID Callback
 router.post('/callback', smileIDCallback)
